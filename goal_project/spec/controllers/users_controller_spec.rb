8476c4c137964a35c7afb13f8e5a291440f30dea
DOV3D <davidovelazquez3@gmail.com>
2021-09-22 17:32:32 -0400

    Add some user model methods

--- goal_project/spec/controllers/users_controller_spec.rb
+++ goal_project/spec/controllers/users_controller_spec.rb
@@ -4,25 +4,59 @@ RSpec.describe UsersController, type: :controller do
     subject(:user) do 
         User.create!(username: "Feifei", password: "password")
     end 
-    describe 'GET #index'
+    describe 'GET #index' do
         it "renders a index template" do
             get :index
             expect(response).to render_template(:index)
         end 
+    end
     
-    describe 'GET #show'
+    describe 'GET #show' do
         it "renders a show template" do 
             get :show, params: {id: user.id}
             expect(response).to render_template(:show)
         end 
+    end
 
 
-    describe 'POST #create'
-        
+    describe 'POST #create' do
+        context "with invalid params" do
+            it "validates the presence of the user's username and password" do
+            post :create, params: { 
+                user: { 
+                    username: "Feifei", 
+                    password: ""
+                }
+            }
+            expect(response).to render_template("new")
+            expect(flash[:errors]).to be_present
+            end
+            it "validates the password's length is at least 6 characters long" do
+                post :create, params: {
+                    user: {
+                        username: "Feifei",
+                        password: "less"
+                    }
+                }
+            expect(response).to render_template("new")
+            expect(flash[:errors]).to be_present
+            end
+        end
+
+        context "with valid params" do
+            it "redirects user to show on success" do
+                post :create, params: {
+                   user: { 
+                       username: "Feifei", 
+                       password: "password"
+                    }
+                }
+                expect(response).to redirect_to(user_url(User.last))
+            end
+        end
 
 
-
-
+    end
 
 end 
  
